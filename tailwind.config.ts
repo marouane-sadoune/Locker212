cfd44c2470a1c41b2cc0891beab50d2e882121d7
marouane-sadoune <marouanesadoune@gmail.com>
2026-03-26 03:06:57 +0100

    old font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,8 +24,8 @@ export default {
         'warning': '#7C6A2A',
       },
       fontFamily: {
-        serif: ['Syne', 'sans-serif'],
-        sans: ['"DM Sans"', 'sans-serif'],
+        serif: ['"Cormorant Garamond"', 'serif'],
+        sans: ['Jost', 'sans-serif'],
       },
       borderRadius: {
         'none': '0px',
